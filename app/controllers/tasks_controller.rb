45a2c4434741e29665a1f6d24ee39e525a485ff8
Ko Nakayama <ko.nakayama.cnt@gmail.com>
2017-04-21 04:21:02 +0000

    finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -43,8 +43,8 @@ class TasksController < ApplicationController
       @task = Task.find(params[:id])
     @task.destroy
 
-    flash[:success] = 'Message は正常に削除されました'
-    redirect_to messages_url
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_to tasks_url
   end
 end
 
